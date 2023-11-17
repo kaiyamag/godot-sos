9812684f7b0cad76f58e9f77b8d80824247b9aff
kaiyamag <kmagnuson24@wooster.edu>
2023-11-17 15:27:57 -0500

    Test changing shader parameter from controller script

--- Scene/c_controller.cs
+++ Scene/c_controller.cs
@@ -3,10 +3,13 @@ using System;
 
 public partial class c_controller : MeshInstance3D
 {
+	Material mat;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		mat = this.GetSurfaceOverrideMaterial(0);
+		(mat as ShaderMaterial).SetShaderParameter("speed", 10);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
