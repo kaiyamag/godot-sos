c1397404803f07c1330f174601136a940af18020
kaiyamag <kmagnuson24@wooster.edu>
2023-11-17 14:02:03 -0500

    Set up C# script

--- Scene/shader_controller.gd
+++ Scene/shader_controller.gd
@@ -1,12 +1,11 @@
 extends MeshInstance3D
-var Mat
+var mat
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Mat = self.get_surface_override_material(0)
-	Mat.set_shader_parameter("speed", 10)
-	#pass
+	mat = self.get_surface_override_material(0)
+	mat.set_shader_parameter("speed", 10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
