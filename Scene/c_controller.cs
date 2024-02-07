eaa41009f67997c3d24a31c13116c725916eca88
kaiyamag <kmagnuson24@wooster.edu>
2024-02-07 15:52:27 -0500

    Add lerp function to control random wave height

--- Scene/c_controller.cs
+++ Scene/c_controller.cs
@@ -10,16 +10,54 @@ using System;
 public partial class c_controller : MeshInstance3D
 {
 	Material mat;
+	float r1, r2;
+	float random;
+	private int lerp_time;
+	private float lerp_max = 100;
+	private Random rand;
+	
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		lerp_time = 0;
+
+		// Generate pseudorandom numbers
+		rand = new Random();
+		r1 = (float) (rand.Next(0, 100) / 100.0);
+		r2 = (float) (rand.Next(0, 100) / 100.0);
+		random = r1;
+		GD.Print(random);
+
+		// Set shader uniform
 		mat = this.GetSurfaceOverrideMaterial(0);
-		(mat as ShaderMaterial).SetShaderParameter("speed", 10);
+		(mat as ShaderMaterial).SetShaderParameter("rand_factor", random);
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Lerp to a new random target if lerp_max is reached
+		if (lerp_time % lerp_max == 0) {
+			// Reset r1 and r2
+			r1 = r2;
+			r2 = (float) (rand.Next(0, 100) / 100.0);
+			GD.Print("Reset, next r = ", r2);
+		}
+		random = Lerp(r1, r2, lerp_time % lerp_max);
+		GD.Print("r1: ", r1, ", r2: ", r2, ", rand: ", random, ", lerp_time: ", lerp_time);
+
+		// Update shader uniform
+		(mat as ShaderMaterial).SetShaderParameter("rand_factor", random);
+
+		lerp_time++;
 	}
+
+	// Linear interpolation between two floats. 
+	// Code from https://forum.godotengine.org/t/how-to-use-lerp-functions-in-c/15038/2
+	private float Lerp(float firstFloat, float secondFloat, float by)
+{
+     return firstFloat * ((float)1.0 - by) + secondFloat * by;
+}
 }
