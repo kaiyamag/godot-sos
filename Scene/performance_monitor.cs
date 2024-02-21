3518fe4f0682d82324b2a7ef359f184a33b7fc19
kaiyamag <kmagnuson24@wooster.edu>
2024-02-21 17:36:35 -0500

    Add stats label to project build

--- Scene/performance_monitor.cs
+++ Scene/performance_monitor.cs
@@ -4,6 +4,7 @@ using System.IO;
 
 public partial class performance_monitor : Node
 {
+	public int sample_rate = 10;					// Number of frames to wait between each profiler sample
 	private double avg_FPS, min_FPS, max_FPS;
 	private double fps;						// FPS at current frame
 	private double fps_sum;					// Accumulator for FPS average
@@ -21,7 +22,7 @@ public partial class performance_monitor : Node
 
 	// Initialize min/max values and accumulators
 	public override void _Ready()
-	{
+	{	
 		avg_FPS = 0;
 		min_FPS = 1000;		// Magic numbers used here since FPS should never exit the range [0,1000]
 		max_FPS = 0;
@@ -44,7 +45,7 @@ public partial class performance_monitor : Node
 	*/
 	public override void _Process(double delta)
 	{
-		int sample_rate = 10;			// Number of frames between samples
+		//int sample_rate = 10;			// Number of frames between samples
 		int print_rate = 500;			// Number of frames between printing stats
 		frame_count++;
 
@@ -55,8 +56,13 @@ public partial class performance_monitor : Node
 			sampleMem();
 		}
 
-		if (frame_count % print_rate == 0) {
+		if (frame_count >= print_rate) {
 			printStats();
+			frame_count = 0;
+			sample_count = 0;
+			fps_sum = 0;
+			time_sum = 0;
+			mem_sum = 0;
 		}
 	}
 
@@ -103,26 +109,38 @@ public partial class performance_monitor : Node
 	* Prints formatted FPS, draw time, and memory usage stats
 	*/
 	private void printStats() {
-		string fstr;	// Placeholder for formatted strings
-		GD.Print("------------------------");
-		fstr = avg_FPS.ToString("F2"); 
-		GD.Print("Avg. FPS: ", fstr);
-		GD.Print("Min FPS: ", min_FPS);
-		GD.Print("Max FPS: ", max_FPS);
+		// string fstr;	// Placeholder for formatted strings
+		// GD.Print("------------------------");
+		// fstr = avg_FPS.ToString("F2"); 
+		// GD.Print("Avg. FPS: ", fstr);
+		// GD.Print("Min FPS: ", min_FPS);
+		// GD.Print("Max FPS: ", max_FPS);
 
-		GD.Print();
-		fstr = toMS(avg_time).ToString("F2"); 
-		GD.Print("Avg. Draw Time: ", fstr, " ms");
-		fstr = toMS(min_time).ToString("F2"); 
-		GD.Print("Min Draw Time: ", fstr, " ms");
-		fstr = toMS(max_time).ToString("F2");
-		GD.Print("Max Draw Time: ", fstr, " ms");
+		// GD.Print();
+		// fstr = toMS(avg_time).ToString("F2"); 
+		// GD.Print("Avg. Draw Time: ", fstr, " ms");
+		// fstr = toMS(min_time).ToString("F2"); 
+		// GD.Print("Min Draw Time: ", fstr, " ms");
+		// fstr = toMS(max_time).ToString("F2");
+		// GD.Print("Max Draw Time: ", fstr, " ms");
 
-		GD.Print();
-		fstr = toMB(avg_mem).ToString("F4");
-		GD.Print("Avg. Memory: ", fstr, " MB");
-		fstr = toMB(mem).ToString("F4");
-		GD.Print("Current Memory: ", fstr, " MB");
+		// GD.Print();
+		// fstr = toMB(avg_mem).ToString("F4");
+		// GD.Print("Avg. Memory: ", fstr, " MB");
+		// fstr = toMB(mem).ToString("F4");
+		// GD.Print("Current Memory: ", fstr, " MB");
+
+		string label_str = "";												// Placeholder for formatted strings 
+		label_str += $"\nAvg. FPS: {avg_FPS.ToString("F0")}"; 
+		label_str += $"\nMin FPS: {min_FPS.ToString("F0")}";
+		label_str += $"\nMax FPS: {max_FPS.ToString("F0")}";
+		label_str += $"\nAvg. Draw Time: {toMS(avg_time).ToString("F2")} ms";
+		label_str += $"\nMin Draw Time: {toMS(min_time).ToString("F2")} ms";
+		label_str += $"\nMax Draw Time: {toMS(max_time).ToString("F2")} ms";
+		label_str += $"\nAvg. Memory: {toMB(avg_mem).ToString("F2")} MB";
+		label_str += $"\nCurrent Memory: {toMB(mem).ToString("F2")} MB";
+
+		GetNode<Label>("Label").Text = label_str;
 	}
 
 	/**
